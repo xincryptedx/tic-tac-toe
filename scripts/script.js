821f4205878a688a8b3d929f2995c9eaac5564a9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-08 16:47:32 -0500

    Add some psudocode to sript.js

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+/*  Event manager: handles sub/unsub to events as well as emitting events
+    .on(eventName, handler)
+    .off(eventName)
+    .emit(eventName)
+
+    Events: object that stores event names for the project
+
+    gameboard module: holds and changes game board data
+    .makeMark(marker, position): attempts to set given marker at given position
+                    
+    player factory: creates player objects
+    props
+    .makeMark: 
+    .quit
+
+
+    computerPlayer: module that inherits from player factory with object.create
+
+*/
