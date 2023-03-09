ebd6494f1af087c4a0a4e6166c8415d58fefb1ad
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-08 19:24:53 -0500

    Move eventManager module into script.js

--- script.js
+++ script.js
@@ -17,3 +17,21 @@
     computerPlayer: module that inherits from player factory with object.create
 
 */
+const EventManager = (() => {
+  const events = {};
+
+  const on = (eventName, fn) => {
+    if (!events[eventName]) {
+      events[eventName] = [];
+    }
+    events[eventName].push(fn);
+  };
+
+  /*   const off = function (eventName, fn) {}; */
+
+  return {
+    on,
+  };
+})();
+
+export default EventManager;
