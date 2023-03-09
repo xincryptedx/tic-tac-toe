03f99a82bb532d970566cb155030b6ba0bcf5117
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-08 19:52:29 -0500

    Remove EventManager
    I cannot figure out how to use this now. Instead of wasting more time
    I'm removing it and going with a simpler solution. I hope.

--- script.js
+++ script.js
@@ -12,37 +12,3 @@
     computerPlayer: module that inherits from player factory with object.create
 
 */
-const EventManager = (() => {
-  const events = {};
-
-  const on = (eventName, fn) => {
-    if (!events[eventName]) {
-      events[eventName] = [];
-    }
-    events[eventName].push(fn);
-  };
-
-  const off = (eventName, fn) => {
-    if (this.events[eventName]) {
-      for (let i = 0; i < this.events[eventName].length; i += 1) {
-        if (this.events[eventName][i] === fn) {
-          this.events[eventName].splice(i, 1);
-          break;
-        }
-      }
-    }
-  };
-
-  const emit = (eventName, data) => {
-    if (this.events[eventName]) {
-      this.events[eventName].forEach((fn) => {
-        fn(data);
-      });
-    }
-  };
-  return {
-    on,
-    off,
-    emit,
-  };
-})();
