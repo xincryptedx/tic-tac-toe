382607514fc617a5964138ad5f60222b08f80eed
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-09 20:12:26 -0500

    Remove video loading from gameBoard

--- script.js
+++ script.js
@@ -16,30 +16,9 @@ const gameBoard = (() => {
   const spots = boardDiv.children;
   const marks = boardDiv.querySelectorAll(".mark");
 
-  const markVideo = document.createElement("video");
-  markVideo.src =
-    "https://drive.google.com/uc?id=1tZ9VppmxwWYUEgaqNbY9v-a8tffZmXuJ";
-
-  marks.forEach((video) => {
-    const newVid = video;
-    newVid.src = markVideo.src;
-  });
-
   const board = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 
-  const render = (position, shouldAnimate = false) => {
-    if (shouldAnimate) {
-    } else {
-      for (let i = 0; i < marks.length; i += 1) {
-        if (board[i] === -1) {
-          marks[i].pause();
-          marks[i].currentTime = 1;
-        }
-        // If 0  set image frame x
-        // If 1 set image frame y
-      }
-    }
-  };
+  const render = (position, shouldAnimate = false) => {};
 
   const makeMark = (marker, position) => {};
 
