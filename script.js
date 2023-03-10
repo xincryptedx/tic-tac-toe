e20c376909da8b2f7c2a24c1b1c5acbf2d737b45
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-09 19:40:28 -0500

    Set mark to frame 0 if space is empty

--- script.js
+++ script.js
@@ -27,19 +27,14 @@ const gameBoard = (() => {
 
   const board = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 
-  const backupRender = () => {
-    for (let i = 0; i < board.length; i += 1) {
-      if (board[i] !== 0) {
-        spots[i].innerHTML = board[i];
-      }
-    }
-  };
-
-  const render = (shouldAnimate, position) => {
+  const render = (position, shouldAnimate = false) => {
     if (shouldAnimate) {
     } else {
       for (let i = 0; i < marks.length; i += 1) {
-        // If -1 set image frame 1
+        if (board[i] === -1) {
+          marks[i].pause();
+          marks[i].currentTime = 1;
+        }
         // If 0  set image frame x
         // If 1 set image frame y
       }
