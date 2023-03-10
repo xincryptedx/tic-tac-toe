faf1d5ceeb47cd6ff46052a95cae5a2c259c513e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-03-09 20:28:21 -0500

    Load marker images and render based on board[]

--- script.js
+++ script.js
@@ -16,9 +16,27 @@ const gameBoard = (() => {
   const spots = boardDiv.children;
   const marks = boardDiv.querySelectorAll(".mark");
 
+  const oMarker = new Image();
+  oMarker.src =
+    "https://drive.google.com/uc?id=1K6q9i9jUrw3XQm40Yi4OvLU0r87Q6pTF";
+  const xMarker = new Image();
+  xMarker.src =
+    "https://drive.google.com/uc?id=1O-Lz3u9B9Zco6p4sQh_KHGhVXqOOljGz";
+
   const board = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 
-  const render = (position, shouldAnimate = false) => {};
+  const render = () => {
+    for (let i = 0; i < marks.length; i += 1) {
+      if (board[i] === 1) {
+        marks[i].src = xMarker.src;
+      }
+      if (board[i] === 0) {
+        marks[i].src = oMarker.src;
+      } else {
+        marks[i].src = "";
+      }
+    }
+  };
 
   const makeMark = (marker, position) => {};
 
